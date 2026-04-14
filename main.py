2bb7aba59dded123e780f1efbb734f9967248bee
yasryameen21-rgb <yasryameen21@gmail.com>
2026-04-14 09:31:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings=["*"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
