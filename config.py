014f5c2b7cc3831fba828115965d6ef79deea808
yasryameen21-rgb <yasryameen21@gmail.com>
2026-04-18 03:01:36 +0000

    Add files via upload

--- config.py
+++ config.py
@@ -19,6 +19,8 @@ DEFAULT_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
+    "https://web-50ne.onrender.com",
+    "https://yamen-yasry.onrender.com",
     "https://yamen-yasry-backend.onrender.com",
 ]
 
